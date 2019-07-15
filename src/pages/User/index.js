c6260573d119509fc769bc866bb1cc7347a841ee
Diego Mais <diegomais@live.com>
2019-07-14 22:33:42 -0300

    Add repository page using WebView
    - This feature provides user to view repository page at GitHub.
    
    Changes to be committed:
    	modified:   android/app/build.gradle
    	modified:   android/app/src/main/java/com/github/MainApplication.java
    	modified:   android/settings.gradle
    	modified:   ios/Podfile
    	modified:   ios/Podfile.lock
    	modified:   package.json
    	new file:   src/pages/Repository/index.js
    	modified:   src/pages/User/index.js
    	modified:   src/pages/User/styles.js
    	modified:   src/routes.js
    	modified:   yarn.lock

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -26,6 +26,7 @@ export default class User extends Component {
   static propTypes = {
     navigation: PropTypes.shape({
       getParam: PropTypes.func,
+      navigate: PropTypes.func,
     }).isRequired,
   };
 
@@ -68,14 +69,21 @@ export default class User extends Component {
   };
 
   refreshList = () => {
-    this.setState({ loading: true, page: 1 });
+    this.setState({ refreshing: true, loading: true, page: 1 });
 
     this.load();
+  };
+
+  handleNavigate = repository => {
+    const { navigation } = this.props;
+
+    navigation.navigate('Repository', { repository });
   };
 
   render() {
     const { navigation } = this.props;
     const { stars, loading, refreshing } = this.state;
+
     const user = navigation.getParam('user');
 
     return (
@@ -97,7 +105,7 @@ export default class User extends Component {
             onRefresh={this.refreshList}
             refreshing={refreshing}
             renderItem={({ item }) => (
-              <Starred>
+              <Starred onPress={() => this.handleNavigate(item)}>
                 <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
                 <Info>
                   <Title>{item.name}</Title>
